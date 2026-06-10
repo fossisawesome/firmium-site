33dd3a9a4fef3d3b60431b763eadf43b14320496
fossisawesome-macbook-with-linux <fossisawesome@github.com>
2026-06-10 19:52:16 -0400

    Configure GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? '/firmium-site/' : '/',
   plugins: [svelte()],
   server: {
     port: 5174,
